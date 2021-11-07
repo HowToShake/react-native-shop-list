c6318ce3e209adf10ed7784d7b8efcdc1ed58358
Michał Ryśkiewicz <michal.ryskiewicz@inclub.io>
2021-11-07 19:39:07 +0100

    [ADDED] SharedPreferences to project

--- android/app/src/main/java/com/miniprojekt1/MainApplication.java
+++ android/app/src/main/java/com/miniprojekt1/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import in.sriraman.sharedpreferences.RNSharedPreferencesReactPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -20,12 +22,14 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+//           packages.add(new RNSharedPreferencesReactPackage());
           return packages;
         }
 
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Text} from 'native-base'
+const SharedPreferences = require('react-native-shared-preferences');
 
 const Settings = () => {
+    useEffect(() => {
+        SharedPreferences.setItem("key","value");
+    })
+
+    SharedPreferences.getItem("key", function(value: any){
+        console.log('valueee', value);
+    });
+
     return(
         <Text>Settings</Text>
     )
